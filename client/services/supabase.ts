256478fa3f00628b4291239e13d9d72b179b360a
Aniket Kakde <aniketkakde509@gmail.com>
2026-02-23 22:37:49 +0530

    fixed pdf parsing issue01

--- client/services/supabase.ts
+++ client/services/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-    console.warn("Supabase URL and Anon Key are not set in environment variables");
+    throw new Error("Supabase URL and Anon Key are not set in environment variables");
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
